a633d848152784ba545323e0153d6642be485576
stephbsb <stephany0303@gmail.com>
2020-08-26 15:11:25 -0300

    create sign up page

--- src/components/login/LoginCreate.js
+++ src/components/login/LoginCreate.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Input from '../form/Input';
+import Button from '../form/Button';
+import Error from '../helper/Error';
+import useForm from '../../hooks/useForm';
+import { USER_POST } from '../../api';
+
+import UserContext from '../../UserContext';
+import useFetch from '../../hooks/useFetch';
 
 const LoginCreate = () => {
-  return <div>LoginCreate</div>;
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm('password');
+  const { userLogin } = useContext(UserContext);
+  const { loading, error, request } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+
+    const { response } = await request(url, options);
+    if (response.ok) {
+      userLogin(username.value, password.value);
+    }
+  }
+  return (
+    <section className='animeLeft'>
+      <h1 className='title'>Create Account</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label='User' type='text' name='username' {...username} />
+        <Input label='Email' type='text' name='email' {...email} />
+        <Input label='Password' type='text' name='password' {...password} />
+        {loading ? (
+          <Button disabled>Signing you up!</Button>
+        ) : (
+          <Button>Sign up...</Button>
+        )}
+        <Error error={error} />
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
